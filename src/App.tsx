f335326856d45530e7fd18b9982801ee4fbfe03a
tomass <tomas.sakocius@asseco.lt>
2024-01-24 10:53:04 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <h1>Simona Grigaraitė</h1>
+      <div className="card">
+        <p>
+         Individuali psichodinaminė psichoterapija.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
